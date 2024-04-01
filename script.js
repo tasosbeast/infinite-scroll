6a76c5c3faed43001fd1a7385bc907f2da493bcd
tasosbeast <66928134+tasosbeast@users.noreply.github.com>
2024-04-02 00:20:54 +0300

    Add Unsplash API integration

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// Unsplash API
+const count = 10;
+const apiKey = "g26280_Enqn8P9DEKwACacG3-9PcG4hZwai2nxaV7AA";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+async function getPhotosFromUnsplashApi() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+// on load
+getPhotosFromUnsplashApi();
