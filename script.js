b24a24b3e595186790ceab0abc2e5ec1c0305563
tasosbeast <66928134+tasosbeast@users.noreply.github.com>
2024-04-02 21:38:03 +0300

    Add infinite scroll functionality

--- script.js
+++ script.js
@@ -85,5 +85,14 @@ async function getPhotosFromUnsplashApi() {
   }
 }
 
+// check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >=
+    document.body.offsetHeight - 1000
+  ) {
+    getPhotosFromUnsplashApi();
+  }
+});
 // on load
 getPhotosFromUnsplashApi();
