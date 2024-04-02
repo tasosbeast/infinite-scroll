cfcf7190bcbc4eb451712a39eabaaece10d0c022
tasosbeast <66928134+tasosbeast@users.noreply.github.com>
2024-04-02 18:07:40 +0300

    Refactor image display and add intersection observer

--- script.js
+++ script.js
@@ -1,13 +1,85 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let photosArray = [];
+
 // Unsplash API
 const count = 10;
 const apiKey = "g26280_Enqn8P9DEKwACacG3-9PcG4hZwai2nxaV7AA";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// helper function to set attributes on DOM elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+// create elements for links & photos, add to dom
+// create elements for links & photos, add to dom
+function displayPhotos() {
+  // run function for each object in photosArray
+  photosArray.forEach((photo) => {
+    // create <a> to link to Unsplash
+    const item = document.createElement("a");
+
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+    // create <img> for photo
+    const img = document.createElement("img");
+
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    // add a data attribute for the color
+    img.dataset.color = photo.color;
+
+    // put <img> inside <a>, then put both inside imageContainer Element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+
+    // create an observer instance
+    const observer = new IntersectionObserver(
+      (entries) => {
+        // loop over the entries
+        entries.forEach((entry) => {
+          // if the element is visible
+          if (entry.isIntersecting) {
+            // change the background color to the color from the data attribute
+            document.body.style.backgroundColor = entry.target.dataset.color;
+            // scale the image
+            entry.target.style.filter = "saturate(100%)";
+            entry.target.style.transition = "all 0.6s ease";
+            entry.target.style.filter = "blur(0px)";
+          } else {
+            // reset the scale when the image is not at the center
+            entry.target.style.filter = "saturate(10%)";
+            entry.target.style.filter = "blur(10px)";
+          }
+        });
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.5,
+      }
+    );
+
+    // start observing the image
+    observer.observe(img);
+  });
+}
+
 async function getPhotosFromUnsplashApi() {
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
+    photosArray = await response.json();
+    console.log(photosArray);
+    displayPhotos();
   } catch (error) {
     // Catch Error Here
   }
